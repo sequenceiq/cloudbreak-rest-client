5ec052515a27f5be099a28645a7ced4767842c27
keyki <keyki.kk@gmail.com>
2014-06-04 09:51:49 +0200

    create client constructor

---
+++ src/main/groovy/com/sequenceiq/cloudbreak/main/CloudbreakClientMain.groovy
@@ -0,0 +1,9 @@
+package com.sequenceiq.cloudbreak.main
+
+import com.sequenceiq.cloudbreak.client.CloudbreakClient
+
+class CloudbreakClientMain {
+    public static void main(String[] args) {
+        def client = new CloudbreakClient()
+    }
+}
