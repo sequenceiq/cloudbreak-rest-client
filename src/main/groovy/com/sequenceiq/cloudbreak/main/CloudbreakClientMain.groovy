71560d07c2aace369b4708df865ee91a4c65b09b
Richard Doktorics <doktoricsrichard@gmail.com>
2015-03-18 09:28:00 +0100

    CLOUD-515 added version check possibility

--- src/main/groovy/com/sequenceiq/cloudbreak/main/CloudbreakClientMain.groovy
+++
@@ -1,9 +0,0 @@
-package com.sequenceiq.cloudbreak.main
-
-import com.sequenceiq.cloudbreak.client.CloudbreakClient
-
-class CloudbreakClientMain {
-    public static void main(String[] args) {
-        def client = new CloudbreakClient()
-    }
-}
